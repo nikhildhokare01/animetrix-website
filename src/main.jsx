e17769aa0a215d680f1d121b95709e99326db442
nikhildhokare01 <nikhildhokare04@gmail.com>
2025-12-13 10:16:59 +0530

    Change import statement to use .jsx extension

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.js' // <-- MUST INCLUDE THE .jsx EXTENSION
+import App from './App.jsx' // <-- MUST INCLUDE THE .jsx EXTENSION
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(<App />)
